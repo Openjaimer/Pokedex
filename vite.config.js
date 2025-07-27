16b5ddf63637e92998ae5663866d527f10d3766b
Openjaimer <mc8224842@gmail.com>
2025-07-26 23:45:36 -0600

    Carousel y Pokedex

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: 'https://github.com/Openjaimer/Pokedex',
+  base: '/https://github.com/Openjaimer/Pokedex/',
   plugins: [
     react(),
     tailwindcss()
